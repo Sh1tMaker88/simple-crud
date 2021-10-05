0d2a69c31a024db7d490b842f2a9bc7ea5d3285e
ItsMaker <25mugl@gmail.com>
2021-10-05 20:47:08 +0300

    Add swagger documentation to project. Close #8

--- src/main/java/com/godel/simplecrud/controller/EmployeeController.java
+++ src/main/java/com/godel/simplecrud/controller/EmployeeController.java
@@ -2,14 +2,21 @@ package com.godel.simplecrud.controller;
 
 import com.godel.simplecrud.model.Employee;
 import com.godel.simplecrud.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
+@Validated
+@Tag(name = "Employee controller", description = "Give access to CRUD operation for employees")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
@@ -19,21 +26,25 @@ public class EmployeeController {
     }
 
     @GetMapping
+    @Operation(summary = "Show all employees", description = "Let see list of all employees")
     public ResponseEntity<List<Employee>> showAllEmployees() {
         return ResponseEntity.ok(employeeService.findAllEmployees());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Employee> showEmployee(@PathVariable Long id) {
+    @Operation(summary = "Show specific employee by his ID", description = "Let see specific employee by his ID as path variable")
+    public ResponseEntity<Employee> showEmployee(@PathVariable @Parameter(description = "Employee ID") Long id) {
         return ResponseEntity.ok(employeeService.findEmployeeById(id));
     }
 
     @PostMapping
+    @Operation(summary = "Create new employee", description = "Let create new employee with given parameters")
     public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
         return ResponseEntity.status(HttpStatus.CREATED).body(employeeService.createEmployee(employee));
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Create or update employee", description = "Let create new employee with given parameters or update if he already exists")
     public ResponseEntity<Employee> updateEmployee(@PathVariable Long id,
                                                    @RequestBody Employee employee) {
         employee.setEmployeeId(id);
@@ -42,7 +53,8 @@ public class EmployeeController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Employee> deleteEmployee(@PathVariable Long id) {
+    @Operation(summary = "Delete employee", description = "Let delete employee by his ID")
+    public ResponseEntity<Employee> deleteEmployee(@PathVariable @Parameter(description = "Employee ID") Long id) {
         employeeService.deleteEmployeeById(id);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
--- src/main/java/com/godel/simplecrud/model/Employee.java
+++ src/main/java/com/godel/simplecrud/model/Employee.java
@@ -1,13 +1,13 @@
 package com.godel.simplecrud.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
-import java.io.Serializable;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Getter
@@ -24,23 +24,31 @@ public class Employee {
     private Long employeeId;
 
     @Column(name = "first_name")
+    @Schema(required = true, pattern = "[A-Z]\\D{2,}")
+    @Pattern(regexp =  "[A-Z]\\D{2,}", message = "First name must start with capital letter and length 3+ letters")
     private String firstName;
 
     @Column(name = "last_name")
+    @Schema(required = true, pattern = "[A-Z]\\D{2,}")
+    @Pattern(regexp =  "[A-Z]\\D{2,}", message = "Last name must start with capital letter and length 3+ letters")
     private String lastName;
 
     @Column(name = "department_id")
+    @Schema(required = true)
+    @Positive(message = "Department ID cannot be null")
     private Long departmentId;
 
     @Column(name = "job_title")
+    @Schema(required = true)
     private String jobTitle;
 
     @Enumerated(value = EnumType.STRING)
     @Column(name = "gender")
+    @Schema(required = true)
     private Gender gender;
 
     @Column(name = "date_of_birth")
+    @Schema(required = true)
     private LocalDate dateOfBirth;
-
 
 }
